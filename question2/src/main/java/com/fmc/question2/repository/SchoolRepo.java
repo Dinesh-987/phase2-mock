e84c488738c90d487664bc4ccb4c677f642f98dd
Dinesh-987 <dinesharide@gmail.com>
2025-04-28 23:37:02 +0530

    Delete question2 directory

--- question2/src/main/java/com/fmc/question2/repository/SchoolRepo.java
+++
@@ -1,9 +0,0 @@
-package com.fmc.question2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fmc.question2.entity.School;
-
-public interface SchoolRepo extends JpaRepository<School, Long> {
-
-}
